711bbfce7e9efa6fc20b05d8edac290eaa0f459e
Tobias Leugger <leugger.tobias@web.de>
2013-12-29 17:42:16 +0100

    very basic view switch with ui-router

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 (function() {
     'use strict';
 
-    var jamthegame = angular.module('jamthegame', ['ui.bootstrap']);
+    var jamthegame = angular.module('jamthegame', ['ui.bootstrap', 'ui.router']);
+
+    jamthegame.config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider.state('tasks', {
+            url: '/'
+        });
+
+        $stateProvider.state('map', {
+            url: '/map'
+        });
+    });
 
 
     jamthegame.provider('taskProvider', function() {
@@ -38,7 +50,9 @@
         };
     });
 
-    jamthegame.controller('AppCtrl', function($scope, taskProvider) {
+    jamthegame.controller('AppCtrl', function($scope, $state, taskProvider) {
+        $scope.$state = $state;
+
         $scope.tasks = taskProvider.tasks;
 
         $scope.newTaskName = '';
@@ -74,12 +88,20 @@
                 numMaps += 1;
                 elem.attr('id', mapId);
 
-                var map = L.map(mapId);
+                var map = L.map(mapId, {
+                    touchZoom: false,
+                    scrollWheelZoom: false,
+                    doubleClickZoom: false,
+                    boxZoom: false,
+                    zoomControl: false
+                });
 
                 map.setView([46.948006, 7.444689], 13);
 
                  L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="http://openstreetmap.org">OSM</a> contribs, <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OSM</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+                     attribution: '&copy; <a href="http://openstreetmap.org">OSM</a> contribs, <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OSM</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+                     maxZoom: 13,
+                     minZoom: 13
                 }).addTo(map);
 
                 L.marker([46.948006, 7.444689]).addTo(map);
